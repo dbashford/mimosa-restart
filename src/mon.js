372c14276b6bdc5b5ea59ebfd97ea862a698d0b8
david.bashford <dbashford@bericotechnologies.com>
2014-05-13 08:49:08 -0400

    fixes #1, using better watcher

--- src/mon.js
+++ src/mon.js
@@ -3,12 +3,10 @@
 "use strict";
 
 var path = require( "path" )
+  , fs = require( "fs" )
   , spawn = require( "child_process" ).spawn
-  , watch = require( "chokidar" )
   , watchDir = process.argv[2]
-  , args = process.argv[3].split( "|%|" )
-  , watchConfig = { persistent: true }
-  , watcher = watch.watch( path.dirname( watchDir ), watchConfig );
+  , args = process.argv[3].split( "|%|" );
 
 // listen for parent process to tell this one to go away
 process.on( "message", function( m ) {
@@ -19,25 +17,32 @@ process.on( "message", function( m ) {
 
 // check if matching directory, if so, let parent process know
 // it needs to restart, then exit this process
-var onunlinkDir = function( dirName ) {
-  if ( watchDir === dirName) {
-    // make current mimosa stop watching/shutdown/restart
-    process.send( "RESTART_MIMOSA" );
+var restart = function() {
+  // make current mimosa stop watching/shutdown/restart
+  process.send( "RESTART_MIMOSA" );
 
-    // start new mimosa
-    var newMimosa = spawn("node",
-      args,
-      {
-        detached: true,
-        stdio:"inherit"
-      }
-    );
-    newMimosa.unref();
+  // start new mimosa
+  var newMimosa = spawn("node",
+    args,
+    {
+      detached: true,
+      stdio:"inherit"
+    }
+  );
+  newMimosa.unref();
 
-    process.nextTick( function() {
-      process.exit( 0 );
-    });
-  }
+  process.nextTick( function() {
+    process.exit( 0 );
+  });
 };
 
-watcher.on( "unlinkDir", onunlinkDir );
+var PathWatcher = require( "pathwatcher" );
+PathWatcher.watch(watchDir, function(event) {
+  if ( [ "delete", "rename" ].indexOf( event ) > -1 ) {
+    if ( !fs.existsSync( watchDir ) ) {
+      /* eslint no-console:0 */
+      console.log( "Restarting Mimosa...");
+      restart();
+    }
+  }
+});
